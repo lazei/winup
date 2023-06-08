4346b1960d7fb3a8ec037480ff32742f00b0ddf6
fochz <135950155+fochz@users.noreply.github.com>
2023-06-08 07:46:15 +0000

    Update main.yml

--- cmd/0.PatchDownload.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF&(CD /D "%~DP0")&(FLTMC>NUL)||(mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&&exit /b)
-
-set "aria2=bin\aria2c.exe"
-set "patchDir=patch"
-
-echo Patch Downloading...
-"%aria2%" --no-conf --check-certificate=false -x16 -s16 -j5 -c -R -d"%patchDir%" -i"Scripts\script_17763_x64.txt"
--- cmd/1.ExtractingISO.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF&(CD /D "%~DP0")&(FLTMC>NUL)||(mshta vbscript:CreateObject^("Shell.Application"^).ShellExecute^("%~snx0"," %*","","runas",1^)^(window.close^)&&exit /b)
-
-set "a7z=bin\7z.exe"
-set "ISODir=ISO"
-
-::dir /b /a:-d *.iso 1>nul 2>nul && (for /f "delims=" %%# in ('dir /b /a:-d *.iso') do set "isofile=%%#")
-dir /b /a:-d %USERPROFILE%\desktop\*.iso 1>nul 2>nul && (for /f "delims=" %%# in ('dir /b /a:-d %USERPROFILE%\desktop\*.iso') do set "isofile=%%#")
-%a7z% x "%USERPROFILE%\desktop\%isofile%" -o"%~dp0%ISODir%" -r
